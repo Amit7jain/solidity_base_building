2d6bea046afb8d126cb5da7b1b029f60503d557b
Amit7jain <amit7.jain@gmail.com>
2023-01-02 00:18:08 +0530

    Update soldity_functions.sol
    Added Modifier examples ( With class)

--- soldity_functions.sol
+++ soldity_functions.sol
@@ -1,6 +1,21 @@
 //    SPDX-License-Identifier: MIT
 pragma solidity ^0.7.5;
 contract function_test{
+/*
+Function Syntax:
+function function-name(parameter-list) scope returns() {
+   //statements
+}    
+*/
+
+// Single value return function
+  function getSingleResult() public view returns(uint){
+      uint a = 1; // local variable
+      uint b = 2;
+      uint result = a + b;
+      return result;
+   }
+
 
 //In Solidity, a function can return multiple values as well.     
 function getResult() public view returns(uint product, uint sum){
@@ -13,4 +28,47 @@ function getResult() public view returns(uint product, uint sum){
       //multiple values
       //return(a*b, a+b);
    }
+
+// MODIFIER : This is used to modify the behaviour of a function 
 }   
+
+contract Owner {
+    address owner ;
+    constructor() public {
+        owner=msg.sender;
+    }
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier costs (uint price){
+        if(msg.value >= price){
+            _;
+        }
+    }
+
+}
+
+contract Register is Owner {
+    mapping (address =>bool) registerdAddresses;
+    uint price;
+    constructor (uint initialPrice) public {
+        price = initialPrice;
+    }
+
+    function register() public payable costs(price){
+        registerdAddresses[msg.sender] = true;
+
+    }
+// Only owner can execute this function
+    function changePrice(uint _price) public onlyOwner{
+        price = _price;
+    }
+
+
+
+
+}
+
+
