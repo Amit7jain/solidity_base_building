5a9e1986b81f36f6f77e7843779f6d8e37aec457
Amit7jain <amit7.jain@gmail.com>
2023-01-17 04:41:52 +0530

    Update fun_visibility.sol
    Added details on function visibility

--- fun_visibility.sol
+++ fun_visibility.sol
@@ -1,5 +1,18 @@
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+
+/*
+Functions and state variables have to declare whether they are accessible by other contracts.
+
+Functions can be declared as
+
+public - any contract and account can call
+private - only inside the contract that defines the function
+internal- only inside contract that inherits an internal function
+external - only other contracts and accounts can call
+*/
 
 contract Base {
     // Private function can only be called
